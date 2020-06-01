1d25690dea5f3e48e674bf561d0f2f9383d63584
hamicch <hamichtech@gmail.com>
2020-06-01 13:03:35 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+[
+        {
+                plan_features: 'Loan',
+                bronze: URL('check.png'),
+                silver: require('@/check-blue.png'),
+                gold: require('@/check-purple.png'),
+        },
+        {
+                plan_features: 'Loan',
+                bronze: require('@/check-green.png'),
+                silver: require('@/check-blue.png'),
+                gold: require('@/check-purple.png'),
+        }
+]
+
+
+const list = document.querySelector('.list-group');
+const li = document.createElement('li');
+li.className = 'list-item'
+li.appendChild()
